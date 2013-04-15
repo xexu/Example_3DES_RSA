21956e5b5925957943252a3e1b7fb74906b3bb7f
Christian <xexuDj@gmail.com>
2013-04-16 01:05:33 +0200

    Encryption 3DES / RSA using c# and openssl.net

---
+++ ConsoleApplication1/Cryptography.cs
@@ -0,0 +1,137 @@
+﻿using OpenSSL.Crypto;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public static class Cryptography
+{
+    /*
+     * 
+     * Add openssl.net 0.5 reference (Might work with other version)
+     * Copy libeay32.dll and ssleay32.dll to current folder/path
+     * (Might have to) Set Project/Properties/Build Target Platform to x86
+     * 
+     */
+    #region MD5
+
+    public static string HashMD5(string phrase)
+    {
+        if (phrase == null)
+            return null;
+        var encoder = new UTF8Encoding();
+        var md5Hasher = new MD5CryptoServiceProvider();
+        var hashedDataBytes = md5Hasher.ComputeHash(encoder.GetBytes(phrase));
+        return ByteArrayToHexString(hashedDataBytes);
+    }
+
+    #endregion
+
+    #region RSA
+
+    public static byte[] EncryptRSA(string publicKeyAsPem, string payload, string passphrase = null)
+    {
+        var encoder = new UTF8Encoding();
+        byte[] byte_payload = encoder.GetBytes(payload);
+        CryptoKey d = CryptoKey.FromPublicKey(publicKeyAsPem, passphrase);
+        OpenSSL.Crypto.RSA rsa = d.GetRSA();
+        byte[] result = rsa.PublicEncrypt(byte_payload, OpenSSL.Crypto.RSA.Padding.PKCS1);
+        rsa.Dispose();
+        return result;
+    }
+    public static string DecryptRSA(string privateKeyAsPem, byte[] payload, string passphrase = null)
+    {
+        var encoder = new UTF8Encoding();
+        byte[] byte_payload = payload;
+        CryptoKey d = CryptoKey.FromPrivateKey(privateKeyAsPem, passphrase);
+        OpenSSL.Crypto.RSA rsa = d.GetRSA();
+        byte[] result = rsa.PrivateDecrypt(byte_payload, OpenSSL.Crypto.RSA.Padding.PKCS1);
+        rsa.Dispose();
+        return encoder.GetString(result);
+    }
+
+    #endregion
+
+    #region 3DES
+
+    public static string EncryptTripleDES(string phrase, string key, bool hashKey = true)
+    {
+        var keyArray = HexStringToByteArray(hashKey ? HashMD5(key) : key);
+        var toEncryptArray = Encoding.UTF8.GetBytes(phrase);
+
+        var tdes = new TripleDESCryptoServiceProvider
+        {
+            Key = keyArray,
+            Mode = CipherMode.ECB,
+            Padding = PaddingMode.PKCS7
+        };
+
+        var cTransform = tdes.CreateEncryptor();
+        var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+        tdes.Clear();
+        return ByteArrayToHexString(resultArray);
+    }
+
+    public static string DecryptTripleDES(string hash, string key, bool hashKey = true)
+    {
+        var keyArray = HexStringToByteArray(hashKey ? HashMD5(key) : key);
+        var toEncryptArray = HexStringToByteArray(hash);
+
+        var tdes = new TripleDESCryptoServiceProvider
+        {
+            Key = keyArray,
+            Mode = CipherMode.ECB,
+            Padding = PaddingMode.PKCS7
+        };
+
+        var cTransform = tdes.CreateDecryptor();
+        var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+        tdes.Clear();
+        return Encoding.UTF8.GetString(resultArray);
+    }
+
+    #endregion
+
+    #region Helpers
+
+    internal static string ByteArrayToHexString(byte[] inputArray)
+    {
+        if (inputArray == null)
+            return null;
+        var o = new StringBuilder("");
+        for (var i = 0; i < inputArray.Length; i++)
+            o.Append(inputArray[i].ToString("X2"));
+        return o.ToString();
+    }
+
+    internal static byte[] HexStringToByteArray(string inputString)
+    {
+        if (inputString == null)
+            return null;
+
+        if (inputString.Length == 0)
+            return new byte[0];
+
+        if (inputString.Length % 2 != 0)
+            throw new Exception("Hex strings have an even number of characters and you have got an odd number of characters!");
+
+        var num = inputString.Length / 2;
+        var bytes = new byte[num];
+        for (var i = 0; i < num; i++)
+        {
+            var x = inputString.Substring(i * 2, 2);
+            try
+            {
+                bytes[i] = Convert.ToByte(x, 16);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Part of your \"hex\" string contains a non-hex value.", ex);
+            }
+        }
+        return bytes;
+    }
+
+    #endregion
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "hello cryptoworld";
+            
+            #region 3des
+            string pass3des = "3des";
+            System.Console.WriteLine("+3DES");
+            System.Console.WriteLine("Encrypt: " + message + " using pass: " + pass3des);
+            string enc3des = Cryptography.EncryptTripleDES(message, pass3des);
+            System.Console.WriteLine("Result: " + enc3des);
+            System.Console.WriteLine("Decrypt: " + enc3des + " using pass: " + pass3des);
+            string dec3des = Cryptography.DecryptTripleDES(enc3des, pass3des);
+            System.Console.WriteLine("Result: " + dec3des);
+            System.Console.WriteLine((dec3des == message) ? "OK" : "ERROR");
+            System.Console.WriteLine("-3DES");
+            #endregion
+            System.Console.Write("Press enter..."); System.Console.ReadLine();
+            #region rsa
+            String pub = @"-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDiDQQq+dXdxKIJpVNbj7Irn3Cs
+q9ADg4btsT5i9n+ECgYGfMI1WeuzoH5jkoFpvCvQRTyWqrilkbHE4aw72kG2lt/s
+ZqKdQSTcmmyrGpdfJnEqSLEinlo3imO4ZvA+jUhY7IeUa7dzeJ/96zznMGgtwNHL
+tGadMOdC9IisoU0zywIDAQAB
+-----END PUBLIC KEY-----";
+            String priv = @"-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQDiDQQq+dXdxKIJpVNbj7Irn3Csq9ADg4btsT5i9n+ECgYGfMI1
+WeuzoH5jkoFpvCvQRTyWqrilkbHE4aw72kG2lt/sZqKdQSTcmmyrGpdfJnEqSLEi
+nlo3imO4ZvA+jUhY7IeUa7dzeJ/96zznMGgtwNHLtGadMOdC9IisoU0zywIDAQAB
+AoGAby6LwD+Fi0AtmqTwLPauYXpYRvVyqGfGs54b2zz2VxE/CW3Pb8I8hesIfx0N
+/d2JhzSmqkxp6TWRxA5jUkT1UFac9ignE6YqUjaseGGjE285b8mtn2hOwTNHujse
+EK+tK+A6aJpnNwn2DdXKYc4ueZB2YybWYCBBnm4820MZ/VECQQDxNvFmHZ+CX2dD
+ligxR4dGJ0Z/hTxEx/SZjWnmqE/9CBLBm2lkKc5bu7KyBVqIN4Tm5PV5Y/PkrZHa
+V5mTo7R3AkEA7+gg6G4GVBh68DT66bqrZiKJlqZO2axc0LZugBaI4IuJCx7tH6qb
+DI3zaze2JCEosWPTaMqZfQG2L7AuU4Z0TQJAPPzBSCpRPCtW9pWuj9cf8rLXdkJ/
+nHxZ8cD5d6Iypy01YNIkcXjIfhUU90G3RB2VcrONBSYqcjUYXXYslFGdvQJAJ2cs
+4IARsgZDSuiovXLXa/MIPiIamU3iALW1+Hu7B4Zjf9wYfjb7OFioPlfsJor7sAcB
+VhjQlOOPFM4PDdDrkQJBAJucAfsH+Ex7dnd7hTmKRNdDdENqjYr4eWSRJ36wKuVu
+hUgsrgQNQwG8LSpWpLpFrDma+YHzrUlHdxKgMuGZEq8=
+-----END RSA PRIVATE KEY-----";
+            System.Console.WriteLine("+RSA");
+            System.Console.WriteLine("Encrypt: " + message + " using public key");
+            byte[] encrsa_bytes = Cryptography.EncryptRSA(pub, message);
+            System.Console.WriteLine("Decrypt: byte[] using private key");
+            string decrsa = Cryptography.DecryptRSA(priv, encrsa_bytes);
+            System.Console.WriteLine("Result: " + decrsa);
+            System.Console.WriteLine((decrsa == message) ? "OK" : "ERROR");
+            System.Console.WriteLine("-RSA");
+            #endregion
+            System.Console.Write("Press any key..."); System.Console.ReadKey();
+        }
+    }
+}
